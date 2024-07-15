ad4a8831f887377e53656ea5c7cc34769fdd0e7a
AndreaLlerena2003 <98197581+AndreaLlerena2003@users.noreply.github.com>
2024-07-14 21:32:03 -0500

    feat(header): navbar completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './shared/Header/Header';
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,22 +1,35 @@
 import LogoInStudio from '../../assets/LogoInStudio.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {FaBars,FaTimes} from 'react-icons/fa';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './Header.css';
 function Header(){
     return(
-       <header>
-            <h3>Logo</h3>
-            <nav>
-                <a href='/#'>Home</a>
-                <a href='/#'>Home</a>
-                <a href='/#'>Home</a>
-                <button>
-                    <FaTimes/>
-                </button>
-            </nav>
-            <button>
-                <FaBars/>
+       <nav className="navbar navbar-expand-lg navbar-light bg-light">
+         <div className='container-fluid'>
+            <a className="navbar-brand" href="#">
+             <img src={LogoInStudio} alt="Logo" width="50" height="50" />
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                 <span class="navbar-toggler-icon"></span>
             </button>
-       </header>
+            <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+                <ul className='navbar-nav me-auto mb-2 mb-lg-0 color-items'> 
+                    <li className='nav-item'> 
+                        <a className='nav-link color-items'>About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a className="nav-link color-items" aria-current="page" href="#">Become Partner</a>
+                    </li>
+                    <li className='nav-item'> 
+                        <a className='nav-link color-items'>Terms & Conditions</a>
+                    </li>
+                    <li>
+                        <a className='nav-link color-items'>Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+         </div>
+       </nav>
     )
 }
 
