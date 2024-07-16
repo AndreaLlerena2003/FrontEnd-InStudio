be1c4986b9bb9e2d876f84d44b555b117aea4475
AndreaLlerena2003 <98197581+AndreaLlerena2003@users.noreply.github.com>
2024-07-16 08:53:40 -0500

    feat(section-row): row for popular salons

--- src/shared/pages/Home/Home.jsx
+++ src/shared/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import HeroSection from './components/HeroSection/HeroSection.jsx';
-
+import SectionRow from './components/SectionRow/SectionRow.jsx';
+import React from 'react';
 
 function Home(){
     return(
-        <HeroSection/>
+        <React.Fragment>
+            <HeroSection/>
+            <SectionRow/>
+        </React.Fragment>
     )
 }
 
