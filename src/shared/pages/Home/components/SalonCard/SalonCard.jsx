1b05aeb179f732c2909da0fde54ea2754b393f30
AndreaLlerena2003 <98197581+AndreaLlerena2003@users.noreply.github.com>
2024-07-17 20:52:42 -0500

    feat(home): card css

--- src/shared/pages/Home/components/SalonCard/SalonCard.jsx
+++ src/shared/pages/Home/components/SalonCard/SalonCard.jsx
@@ -7,7 +7,7 @@ function SalonCard(props){
             <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <a href="#" className="btn btn-primary b-card">Go somewhere</a>
             </div>
         </div>
     )
