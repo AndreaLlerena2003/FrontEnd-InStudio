ead183193b115b324f6aa5e316f9f8190e9be6ea
AndreaLlerena2003 <98197581+AndreaLlerena2003@users.noreply.github.com>
2024-07-17 20:51:06 -0500

    feat(home): spa card

--- src/shared/pages/Home/Home.jsx
+++ src/shared/pages/Home/Home.jsx
@@ -2,6 +2,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import HeroSection from './components/HeroSection/HeroSection.jsx';
 import SectionRow from './components/SectionRow/SectionRow.jsx';
+import SalonCard from './components/SalonCard/SalonCard.jsx';
+import SpaAsset from '../../../assets/Spa_Asset.png';
 import React from 'react';
 
 function Home(){
@@ -9,6 +11,7 @@ function Home(){
         <React.Fragment>
             <HeroSection/>
             <SectionRow/>
+            <SalonCard image={SpaAsset}/>
         </React.Fragment>
     )
 }
---
+++ src/shared/pages/Home/components/SalonCard/SalonCard.jsx
@@ -0,0 +1,16 @@
+
+
+function SalonCard(props){
+    return(
+        <div className="card">
+            <img src={props.image} className="card-img-top"></img>
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    )
+}
+
+export default SalonCard;
