4e58b8583ea469e17eebed9b5b85cc3e005838b7
AndreaLlerena2003 <98197581+AndreaLlerena2003@users.noreply.github.com>
2024-07-21 17:33:09 -0500

    feat(home): add serviceGrid

--- src/shared/pages/Home/components/ServicesSection/components/Service/Service.jsx
+++ src/shared/pages/Home/components/ServicesSection/components/Service/Service.jsx
@@ -2,13 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import HaircutImage from '../../../../../../../assets/Haircut_Service.png';
 
-function Service(){
+function Service({image,title,description}){
     return(
         <div className='card custom-card-service'>
-             <img src={HaircutImage} class="card-img-top" alt="..."/>
+             <img src={image} class="card-img-top mx-auto d-block" style={{width: '5rem',height:'5rem'} }alt="..."/>
              <div className='card-body'>
-                <h5 class="card-title">Haircut</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 class="card-title title-service">{title}</h5>
+                <p class="card-text text-service">{description}</p>
+                <hr className='hr-service'/>
              </div>
         </div>
     )
