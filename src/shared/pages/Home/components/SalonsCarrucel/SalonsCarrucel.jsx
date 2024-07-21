c6cf8714b9073454296a931798af3fafa0350e9f
AndreaLlerena2003 <98197581+AndreaLlerena2003@users.noreply.github.com>
2024-07-21 11:55:11 -0500

    feat(home): card-slider update

--- src/shared/pages/Home/components/SalonsCarrucel/SalonsCarrucel.jsx
+++ src/shared/pages/Home/components/SalonsCarrucel/SalonsCarrucel.jsx
@@ -2,22 +2,49 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import SalonCard from '../SalonCard/SalonCard';
 import SpaAsset from '../../../../../assets/Spa_Asset.png';
-function SalonCarrucel(props){
-    return(
-        <div className='container text-center mt-2'>
-            <div className='row'>
-                <div className='col-md-4 col-xs-12 mb-3'>
-                    <SalonCard image={SpaAsset}/>
+import React from 'react';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
+
+
+
+function SalonCarrucel(props) {
+    const settings = {
+        dots: true, // puntos de navegacion
+        infinite: false, // carrucel infinito -- false para que no regrese siempre a la primera
+        speed: 500,
+        slidesToShow: 3, // mostrar tres tarjetas al mismo tiempo 
+        slidesToScroll: 1, // mover una tarjeta por vez cuando se navega.
+        swipeToSlide: true, //deslizar al arrastrar con el mouse 
+    };
+
+    return (
+        <div className='container'>
+            <Slider {...settings}>
+                <div>
+                    <SalonCard image={SpaAsset} />
                 </div>
-                <div className='col-md-4 col-xs-12 mb-3'>
-                    <SalonCard image={SpaAsset}/>
+                <div>
+                    <SalonCard image={SpaAsset} />
                 </div>
-                <div className='col-md-4 col-xs-12 mb-3'>
-                    <SalonCard image={SpaAsset}/>
+                <div>
+                    <SalonCard image={SpaAsset} />
                 </div>
-            </div>
+                <div>
+                    <SalonCard image={SpaAsset} />
+                </div>
+                <div>
+                    <SalonCard image={SpaAsset} />
+                </div>
+                <div>
+                    <SalonCard image={SpaAsset} />
+                </div>
+            </Slider>
         </div>
-    )
+    );
 }
+
+
 
 export default SalonCarrucel;
