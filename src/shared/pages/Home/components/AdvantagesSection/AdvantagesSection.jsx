5e39350dab4ddae3f00cb5c865a239e3cb0542ae
AndreaLlerena2003 <98197581+AndreaLlerena2003@users.noreply.github.com>
2024-07-21 17:45:50 -0500

    feat(home): card advantage design

---
+++ src/shared/pages/Home/components/AdvantagesSection/AdvantagesSection.jsx
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+function AdvantagesSection(){
+    return(
+        <div></div>
+    )
+}
+
+export default AdvantagesSection;
---
+++ src/shared/pages/Home/components/AdvantagesSection/components/AdvantageCard.jsx
@@ -0,0 +1,15 @@
+
+
+function AdvantageCard(){
+    return(
+        <div className="hstack gap-3">
+            <img></img>
+            <div className="vstack gap-2">
+                <h6>Titulo de ADV</h6>
+                <p>Descrpcion lalalalalalalalalalallal</p>
+            </div>
+        </div>
+    )
+}
+
+export default AdvantageCard;
