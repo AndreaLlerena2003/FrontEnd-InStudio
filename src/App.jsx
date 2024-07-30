e2c13dfecc3f4f31bda4645f7493eae726d0b65f
AndreaLlerena2003 <20201173@aloe.ulima.edu.pe>
2024-07-29 21:26:19 -0500

    feat/signIn

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './shared/Header/Header';
 import Home from './shared/pages/Home/Home';
 import FooterSection from './shared/Footer/Footer';
 import SignUp from './user/pages/SignUp/SignUp';
+import SignIn from './user/pages/SignIn/SignIn';
 function App() {
   return (
     <Router>
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/home" element={<Home />}/>
           <Route path="/signUp" element={<SignUp/>}/>
+          <Route path="/signIn" element={<SignIn/>}/>
         </Routes>
         <FooterSection/>
       </div>
